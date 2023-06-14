82fe0db667411069932ffd5412742d698f227695
Mark Standen <no-reply@rundog.dev>
2023-06-14 13:42:15 +0100

    added poetry club solution

--- poetry-club-door-policy/door-policy.js
+++ poetry-club-door-policy/door-policy.js
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+    return line[0]
 }
 
 /**
@@ -38,7 +38,7 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+    return capitaliseFirstLetterOnly(word)
 }
 
 /**
@@ -49,7 +49,8 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+    let trimmed = line.trimEnd()
+    return trimmed[trimmed.length - 1]
 }
 
 /**
@@ -60,5 +61,15 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+    return capitaliseFirstLetterOnly(word) + ", please"
+}
+
+/**
+ * Capitalises the first letter of a passed string.
+ * The remaining string is returned in lowercase.
+ * @param phrase The phrase to adjust the case of.
+ * @returns {string} the new string with a capital letter to begin, and the remainder in lowercase.
+ */
+function capitaliseFirstLetterOnly(phrase){
+    return phrase[0].toUpperCase() + phrase.slice(1).toLowerCase()
 }
