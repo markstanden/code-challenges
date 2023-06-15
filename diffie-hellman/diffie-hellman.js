5451871277a4f527ef4fe9453b9edc282f83950d
Mark Standen <no-reply@rundog.dev>
2023-06-15 13:53:00 +0100

    all tests passing

--- diffie-hellman/diffie-hellman.js
+++ diffie-hellman/diffie-hellman.js
@@ -4,15 +4,52 @@
 //
 
 export class DiffieHellman {
-  constructor(p, g) {
-    throw new Error('Remove this statement and implement this function');
-  }
 
-  getPublicKey(privateKey) {
-    throw new Error('Remove this statement and implement this function');
-  }
+    constructor(p, g) {
+        if ((p >= 9999) || (g >= 9999)) {
+            throw Error("prime out of permitted range")
+        }
 
-  getSecret(theirPublicKey, myPrivateKey) {
-    throw new Error('Remove this statement and implement this function');
-  }
+        if ((p < 0) || (g < 0)) {
+            throw Error("Negative value submitted")
+        }
+
+        if (!this.isPrime(p) || !this.isPrime(g)) {
+            throw Error("non-prime values submitted.")
+        }
+
+        /** @type {Number} prime number less than 9999 */
+        this.primeP = p
+
+        /** @type {Number} prime number less than 9999 */
+        this.primeG = g
+    }
+
+    /**
+     * returns true if supplied number is prime
+     * @param number
+     * @returns {boolean}
+     */
+    isPrime(number) {
+        for (let i = 2; i < number; i++) {
+            if ((number % i) === 0) {
+                return false
+            }
+        }
+        return true
+    }
+
+    getPublicKey(privateKey) {
+        if (privateKey <= 1) {
+            throw Error("Private key must have a value greater than 1")
+        }
+        if (privateKey >= this.primeP) {
+          throw Error("Private key cannot equal or greater to the chosen prime 'p'")
+        }
+        return (this.primeG ** privateKey) % this.primeP
+    }
+
+    getSecret(theirPublicKey, myPrivateKey) {
+        return (theirPublicKey ** myPrivateKey) % this.primeP
+    }
 }
