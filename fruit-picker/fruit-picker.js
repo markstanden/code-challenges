35dd7b66da4bf2acbb91da77c38e28672f50aec2
Mark Standen <no-reply@rundog.dev>
2023-06-28 05:09:25 +0100

    callback exercises complete

--- fruit-picker/fruit-picker.js
+++ fruit-picker/fruit-picker.js
@@ -13,17 +13,29 @@ import { notify } from './notifier';
 import { order } from './grocer';
 
 /**
+ * @const
+ * @type {FruitPickerSuccess}
+ */
+const SUCCESS = {message:"SUCCESS"}
+
+/**
+ * @const
+ * @type {FruitPickerError}
+ * */
+const ERROR = {message:"ERROR"}
+
+/**
  * @return void
  */
 export function onSuccess() {
-  // implement the onSuccess callback to call notify with a success message
+    notify(SUCCESS)
 }
 
 /**
  * @return void
  */
 export function onError() {
-  // implement the onError callback to call notify with an error message
+    notify(ERROR)
 }
 
 /**
@@ -33,7 +45,7 @@ export function onError() {
  * @return void
  */
 export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
-  // implement the orderFromGrocer function to order the query
+    order(query, onSuccessCallback, onErrorCallback)
 }
 
 /**
@@ -42,5 +54,5 @@ export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
  * @return void
  */
 export function postOrder(variety, quantity) {
-  //implement the postOrder function to create a query and order
+    orderFromGrocer({variety, quantity}, onSuccess, onError)
 }
