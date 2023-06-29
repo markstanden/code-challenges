4f1f4cecdf297485ccc73979554ac35e9d1fdca4
Mark Standen <no-reply@rundog.dev>
2023-06-29 04:48:05 +0100

    first two classes written with prototypal methods

--- windowing-system/windowing-system.js
+++ windowing-system/windowing-system.js
@@ -1,7 +1,27 @@
 // @ts-check
 
 /**
- * Implement the classes etc. that are needed to solve the
- * exercise in this file. Do not forget to export the entities
- * you defined so they are available for the tests.
+ *
+ * @param {number} width
+ * @param {number} height
+ * @constructor
  */
+export function Size(width = 80, height = 60) {
+    this.width = width
+    this.height = height
+}
+
+Size.prototype.resize = function (newWidth, newHeight) {
+    this.height = newHeight
+    this.width = newWidth
+}
+
+export function Position(x = 0, y = 0) {
+    this.x = x
+    this.y = y
+}
+
+Position.prototype.move = function (newX, newY) {
+    this.x = newX
+    this.y = newY
+}
