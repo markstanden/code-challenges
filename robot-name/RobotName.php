d188abc23af86c04dc3a23c737ec6ab6e1e7d560
Mark Standen <no-reply@rundog.dev>
2023-07-06 04:43:56 +0100

    completed robot_name, had fun with many concepts here

--- robot-name/RobotName.php
+++ robot-name/RobotName.php
@@ -24,12 +24,29 @@
 
 declare(strict_types=1);
 
+/** The ASCII code representing the chr A */
 const ASCII_A = 833;
+
+/** The ASCII code representing the chr Z */
 const ASCII_Z = 858;
 
 class Robot
 {
-    private $name = "";
+    /**
+     * The robot's issued name.
+     * @var string
+     */
+    private string $name;
+    /**
+     * List of previously issued names
+     * @var array
+     */
+    static array $used_names = [];
+
+    public function __construct()
+    {
+        $this->name = $this->createName();
+    }
 
     /**
      * Returns a random name in the format of
@@ -42,10 +59,64 @@ class Robot
         return $this->name;
     }
 
+    /**
+     * Generates a new, random robot name.
+     * name is in the following format:
+     * AA000
+     * @return string
+     */
+    private function generateName(): string
+    {
+        $letters = $this->getLetters(2);
+        $numbers = sprintf("%03d", rand(0, 999));
+        return $letters . $numbers;
+    }
+
+    /**
+     * Adds the name to the list of previously issued names.
+     * @param string $name
+     * @return void
+     */
+    private function addToUsedNames(string $name)
+    {
+        array_push(self::$used_names, $name);
+    }
+
+    /**
+     * Checks whether the generated name has already been issued.
+     * @param string $name
+     * @return bool
+     */
+    private function isDuplicate(string $name): bool
+    {
+        return in_array($name, self::$used_names);
+    }
+
+    /**
+     * returns a unique robot name by generating a new name
+     * and testing whether it has been previously issued.
+     * @return string
+     */
+    private function createName(): string
+    {
+        $new_name = $this->generateName();
+        while ($this->isDuplicate($new_name)) {
+            $new_name = $this->generateName();
+        }
+        $this->addToUsedNames($new_name);
+        return $new_name;
+    }
+
+    /**
+     * Returns the number required of random uppercase letters
+     * @param $number_required
+     * @return string
+     */
     public function getLetters($number_required): string
     {
+        $letter_count = 0;
         $result = "";
-        foreach (range(1, $number_required) as $index) {
+        while (++$letter_count <= $number_required) {
             $result .= chr(rand(ASCII_A, ASCII_Z));
         }
         return $result;
@@ -57,8 +128,6 @@ class Robot
      */
     public function reset(): void
     {
-        $letters = $this->getLetters(2);
-        $numbers = sprintf("%03d", rand(0, 999));
-        $this->name = $letters . $numbers;
+        $this->name = $this->createName();
     }
 }
