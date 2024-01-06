52b8ba8bcde2177908d2eccfdb59f40ddd5d970f
Mark Standen <no-reply@rundog.dev>
2024-01-06 08:31:21 +0000

    [300] Passes all tests

--- 300_longest_increasing_subsequence/longest_increasing_subsequence.js
+++ 300_longest_increasing_subsequence/longest_increasing_subsequence.js
@@ -3,36 +3,66 @@
  * @return {number}
  */
 function lengthOfLIS(nums) {
-    //[10,9,2,5,3,7,101,18]
+    return [
+        ...nums
+            .map(pairWithValidNextSteps)
+            .reverse()
+            .reduce(toMapOfMaxLengths, new Map())
+            .values()
+    ].reduce(max);
 
-    //first scan - find nearest neighbour
-    // 10 -> 101
-    // 9 -> 101
-    // 2 -> 5
-    // 5 -> 7
-    // 3 -> 7
-    // 7 -> 101
-    // 101 -> undefined
-    // 18 -> undefined
+    /**
+     * @typedef {number} key - key
+     * @typedef {number[]} value - Array of valid next steps
+     * @typedef {[key, value]} NextStepsPair
+     */
 
-    const asPairs =  nums.map((value, index, array) => {
-        const next = array.slice(index)
-            .find(next => next > value)
-        return [value, next]
-    })
-
-    asMap = new Map(asPairs)
-
-    function getLength(key, length = 1) {
-        return asMap.get(key) ? getLength(asMap.get(key), length + 1) : length;
+    /**
+     * returns an array of the original value paired with
+     * all valid next steps as an array of pairs
+     * @param {number} current
+     * @param {number} index
+     * @param {number[]} array
+     * @return {NextStepsPair[]}
+     */
+    function pairWithValidNextSteps(current, index, array) {
+        const nextSteps = array.slice(index)
+            .filter(next => next > current)
+        return [current, nextSteps]
     }
 
-    const max = (prev, curr) => curr > prev ? curr : prev;
+    /**
+     * Returns the highest numeric value returned from
+     * the provided map using only the provided keys
+     * @param {Map<*, number>} map
+     * @param {*} keys
+     * @return {number}
+     */
+    function getHighestValue(map, keys){
+        return keys.reduce((highest, key) => Math.max(map.get(key), highest), 0)
+    }
 
-    return [...asMap.keys()]
-        .map(key => getLength(key))
-        .reduce(max);
-};
+    /**
+     * Reducer that returns the highest value in a numeric array
+     * @param {number} prev
+     * @param {number} curr
+     * @return {number}
+     */
+    function max(prev, curr){
+        return Math.max(prev, curr);
+    }
 
+    /**
+     * Reducer that returns a map where each value represents
+     * the longest substring that can be obtained from the provided key
+     * @param {Map<number, number>} map
+     * @param {number} key
+     * @param {number[]} values
+     * @return {Map<number, number>}
+     */
+    function toMapOfMaxLengths(map, [key, values]) {
+        return map.set(key, getHighestValue(map, values) + 1);
+    }
+}
 
 module.exports = {lengthOfLIS};
