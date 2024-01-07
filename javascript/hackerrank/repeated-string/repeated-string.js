7d37100628a3abe13d94117d75228ca7412a8a83
Mark Standen <no-reply@rundog.dev>
2024-01-07 15:22:41 +0000

    Completed solution

--- javascript/hackerrank/repeated-string/repeated-string.js
+++ javascript/hackerrank/repeated-string/repeated-string.js
@@ -5,7 +5,33 @@
  * @param {number} numberOfChars - the number of chars at which point to stop counting 'a's
  */
 function repeatedString(repeatedString, numberOfChars) {
-    return numberOfChars;
+    const length = repeatedString.length;
+    const getACount = getNumberOfAFrom(repeatedString);
+
+    const repeats = Math.trunc(numberOfChars / length);
+    const extras = numberOfChars % length;
+
+    return (repeats * getACount(length)) + getACount(extras);
+
+    /**
+     * Curried function that returns a function to count
+     * the number of 'a's in the provided string
+     * @param {string} str - The string to count the 'a's in
+     * @return {function(number): number}
+     */
+    function getNumberOfAFrom(str) {
+        /**
+         * Function that returns the number of 'a' chars in a substring of length *max*
+         * @param {number} max - The max length of the substring to consider
+         * @return {number} The number of 'a' chars in the substring
+         */
+        return function getNumberOfATo(max) {
+            return str.split('')
+                .slice(0, max)
+                .filter((letter) => letter === 'a')
+                .length;
+        };
+    }
 }
 
 module.exports = {repeatedString};
